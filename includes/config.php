67a889729a5e9c5a4fa107f68a5abde5d991d670
Katherine Burgan <69947300+Kburgan1006@users.noreply.github.com>
2023-12-10 16:04:26 -0500

    Add files via upload

---
+++ includes/config.php
@@ -0,0 +1,11 @@
+<?php
+
+#PDO login information
+const DBNAME = "student_passwords";
+const DBHOST = "localhost";
+const DBUSER = "passwords_user";
+const DBPASS = "uecn!wo93jdn&dk";
+
+#Key and Inital vector for encryption
+const key_str = '0xDBE65FF657030D06DAC37F9BEBC8E62286063FF638AAE8162D2BBD84D3B2FAD2CC78C182B01872A4E511151A7D699444326839F7641DF0A38D0362E92AD6C1AF';
+const init_vector = '0xE2F0987FDB5D294933D262CA90FDDABF';
